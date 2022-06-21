36ded4ef75145c9aa1850520066dbc6da8622efe
Bozhidar Sotirov <bojkoburgas@abv.bg>
2022-06-21 09:26:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import tkinter
+from tkinter import messagebox
+from main_video import main
+import time
+
+while True:
+  root = tkinter.Tk()
+  root.withdraw()
+
+  messagebox.showwarning("Important Reminder", "Get of that fucking chair and Go drink some water!")
+
+  main()
+  time.sleep(600)
