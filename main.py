2ac3be83fb816965267ef826e945bdb8e730a38e
Bozhidar Sotirov <bojkoburgas@abv.bg>
2022-06-25 12:44:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from tkinter import *
-import tkinter
 from tkinter import messagebox
 from main_video import main
 import time
 
 while True:
-  root = tkinter.Tk()
+  root = Tk()
   root.withdraw()
 
   messagebox.showwarning("Important Reminder", "Get of that fucking chair and Go drink some water!")
